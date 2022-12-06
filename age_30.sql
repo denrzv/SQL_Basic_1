80579536e2ff5e116511b5b31de6b9a25ebc1440
denrzv <voofaa@gmail.com>
2022-12-06 07:37:10 +0300

    hw done

---
+++ age_30.sql
@@ -0,0 +1,3 @@
+select * from PERSONS
+where age > 30
+order by age desc;
---
+++ create_table.sql
@@ -0,0 +1,4 @@
+create table PERSONS (
+                         name VARCHAR(100), surname VARCHAR(100), age INTEGER, phone_number INTEGER,
+                          city_of_living VARCHAR(100), primary key (name, surname, age)
+);
